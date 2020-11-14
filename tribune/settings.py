c8feb97d41633b3ada584c4584dbdb47222750fd
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-11-14 10:40:29 +0300

    Deploy to heroku.

--- tribune/settings.py
+++ tribune/settings.py
@@ -100,9 +100,9 @@ if config('MODE')=="dev":
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tribune',
-        'USER': 'moringa',
-        'PASSWORD':'susan2121',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD':config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': '',
     }
