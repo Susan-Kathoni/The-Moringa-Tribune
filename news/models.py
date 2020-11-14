050ffd391a294c96269da0c53393a4d027071810
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-11-14 10:07:30 +0300

    Contains all classes.

---
+++ news/models.py
@@ -0,0 +1,66 @@
+import datetime as dt
+from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField
+
+
+
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
+
+    def __str__(self):
+        return self.first_name
+
+    def save_editor(self):
+        self.save()
+
+
+class tags(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+    def save_tags(self):
+        self.save()
+
+
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor,on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to = 'cloudinary', null=True)
+
+    def __str__(self):
+        return self.title
+
+    def save_new_article(self):
+        self.save()
+
+
+    @classmethod
+    def todays_news(cls):
+        today = dt.date.today()
+        news = cls.objects.filter(pub_date__date = today)
+        return news
+
+
+    @classmethod
+    def days_news(cls,date):
+        news = cls.objects.filter(pub_date__date = date)
+        return news
+
+    
+    @classmethod
+    def search_by_title(cls,search_term):
+        news = cls.objects.filter(title__icontains=search_term)
+        return news
+
+
+class Photo(models.Model):
+  image = CloudinaryField('image')
